fd55715e4d359f97d1d6c51436d528d32f865f00
amalary <amalary@bu.edu>
2025-12-04 13:27:28 -0800

    Move pages under src/pages to align with src/app

--- frontend/src/app/pages/api/profile.ts
+++ frontend/src/app/pages/api/profile.ts
@@ -1,6 +1,6 @@
 // pages/api/profile.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { auth0 } from "../../lib/auth0";
+import { auth0 } from "../../../../lib/auth0";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await auth0.getSession();
